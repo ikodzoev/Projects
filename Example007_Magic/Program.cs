700cec18dfe37120ca9536d44987473b128638ae
ikodzoev <veozdok@gmail.com>
2022-08-29 14:15:40 +0500

    fix Example007 Magic

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -2,16 +2,13 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 1;
-int ya = 1;
-int xb = 1;
-int yb = 30;
-int xc = 80;
-int yc = 30;
+int xa = 1, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
-    
+
 Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+");
 
